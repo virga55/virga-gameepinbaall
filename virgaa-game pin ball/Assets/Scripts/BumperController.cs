0751baffe06ffa93e0a22994db72d16735b1b29e
virga55 <129923659+virga55@users.noreply.github.com>
2023-04-11 00:06:27 +0700

    Ball,Paddle,Bumber

---
+++ virgaa-game pin ball/Assets/Scripts/BumperController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperController : MonoBehaviour
+{
+    public Collider bola;
+    public float multiplier = 2f; // deklarasi variabel multiplier dan menginisialisasi nilainya
+    public Color color;
+
+    private Renderer renderer;
+    private Animator animator;
+
+    private void Start(){
+        renderer = GetComponent<Renderer>();
+        animator= GetComponent<Animator>();
+
+        renderer.material.color=color;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider == bola)
+        {
+            Rigidbody bolaRig = bola.GetComponent<Rigidbody>();
+            bolaRig.velocity *= multiplier; // menggunakan variabel multiplier yang sudah dideklarasikan dan diinisialisasi
+        }
+    }
+}
